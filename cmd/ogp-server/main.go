0bfca5d899baead57d4426754eaf222657d81139
Akira Chiku <akira.chiku@gmail.com>
2021-11-13 09:15:07 +0900

    Update

--- cmd/ogp-server/main.go
+++ cmd/ogp-server/main.go
@@ -43,6 +43,7 @@ func main() {
 			CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
 			PreferServerCipherSuites: false,
 			CipherSuites: []uint16{
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
 				tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
